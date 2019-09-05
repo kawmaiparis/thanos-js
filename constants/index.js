36ed115c36b068e6b5b178bddb820e59a53d71aa
Paris Mongkolkul <paris.kawmai@gmail.com>
2019-09-05 18:08:11 +0700

    clone from OG thanos-js

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	directory: {
+		// The list of directories we should not traverse
+		blacklist: ['node_modules', '.git', '.settings'],
+	},
+	confirmation: {
+		fileName: '_CONFIRMATION_',
+	},
+	arguments: {
+		snapFingers: 'snap-fingers',
+		withGlove: 'with-infinity-gauntlet-glove',
+		noMercy: 'no-mercy',
+	},
+};
